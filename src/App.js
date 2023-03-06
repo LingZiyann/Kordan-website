42b1e87fefd96aa51874bc5457552f497a28e17d
Ling Ziyan <lingziyan2015@gmail.com>
2023-03-06 21:33:45 +0800

    remove uneseccary imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header/Header';
-import Navbar from './Components/Navbar/Navbar';
 import Section1 from './Components/Sections/Section1/Section1';
 import Section2 from './Components/Sections/Section2/Section2';
 import Section3 from './Components/Sections/Section3/Section3';
